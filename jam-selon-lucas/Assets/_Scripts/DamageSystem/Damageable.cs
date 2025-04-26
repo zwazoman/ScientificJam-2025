83026c9d6a2bfacae113eae28273d5de9f1764cd
Yaelp1 <yaelpicot@gmail.com>
2025-04-26 15:08:35 +0200

    MONSTRE PIPELINE

---
+++ jam-selon-lucas/Assets/_Scripts/DamageSystem/Damageable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+    public float hp;
+
+    [SerializeField] bool destroyOnDeath = true;
+
+    public Team team;
+
+    public UnityEvent onDamageTaken, onDeath = new();
+
+    const float invicibilityDuration = .2f;
+    bool CanTakeDamage = true;
+
+    public void TakeDamage(float damage)
+    {
+        if (CanTakeDamage)
+        {
+            hp -= (float)damage;
+            onDamageTaken.Invoke();
+            if (hp <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                StartCoroutine(ApplyInvicibility());
+            }
+        }
+    }
+
+    IEnumerator ApplyInvicibility()
+    {
+        CanTakeDamage = false;
+        yield return new WaitForSeconds(invicibilityDuration);
+        CanTakeDamage = true;
+    }
+
+    public void Die()
+    {
+        onDeath?.Invoke();
+        if (destroyOnDeath)
+        {
+            if (TryGetComponent(out PooledObject pooledObject))
+            {
+                pooledObject.GoBackIntoPool();
+            }
+            else
+            {
+                Debug.LogError("No pooled object attached");
+            }
+        }
+    }
+}
