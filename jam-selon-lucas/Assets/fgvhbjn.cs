3da493c91f8f35fdbc3347c0aa4562fdb8d8ae7e
Yaelp1 <yaelpicot@gmail.com>
2025-04-27 14:50:24 +0200

    MENU

---
+++ jam-selon-lucas/Assets/fgvhbjn.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fgvhbjn : MonoBehaviour
+{
+    public void boule()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void bouleA()
+    {
+        Application.Quit();
+    }
+}
